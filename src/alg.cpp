9f90ee94c6752de113df2324ec0573185261d516
GorenkovPV <94713562+GorenkovPV@users.noreply.github.com>
2022-05-25 16:56:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0, right = 0, left = size - 1;
+    int mid = right + (left - right) / 2;
+    if (true) {
+      for (int right = 0; right < mid; right++) {
+            if (arr[right] == value)
+                count++;
+        }
+      for (int left = size; left >= mid; left--) {
+            if (arr[left] == value)
+                count++;
+        }
+        return count;
+             } else {
+        return 0;
+           }
 }
